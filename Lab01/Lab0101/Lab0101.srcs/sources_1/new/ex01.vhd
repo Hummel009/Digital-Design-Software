f748fa6ed0a0c6e4d36d12ec1ca5a7dd6941fda3
Hummel009 <hummel.turbamentum@gmail.com>
2024-09-16 13:33:52 +0300

    init

---
+++ Lab01/Lab0101/Lab0101.srcs/sources_1/new/ex01.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.09.2024 13:09:12
+-- Design Name: 
+-- Module Name: ex01 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ex01 is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           C : in STD_LOGIC;
+           D : in STD_LOGIC;
+           Q : out STD_LOGIC);
+end ex01;
+
+architecture Behavioral of ex01 is
+
+begin
+
+Q <= A and B and C and D;
+
+end Behavioral;
